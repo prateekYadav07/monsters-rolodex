d46ebb77cdd6a73970ee2125b2eccfc3344f32f8
prateekYadav07 <yadavprateek39@gmail.com>
2023-07-24 17:32:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { Component } from "react";
+import { CardList } from "./components/card-list/CardList.component";
+import { SearchBox } from "./components/searchbox/SearchBox.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }))
+      .catch(() => console.log("requested resource not found"));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonstersList = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monstersList={filteredMonstersList} />
+      </div>
+    );
+  }
+}
+
+export default App;
